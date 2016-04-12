878c7de68d0d1f6ad8da1c9c3fd23c3b39c12c77
BlackDizel <blackdizel@gmail.com>
2016-04-12 15:00:32 +0300

    controller songs get instance method,  play random sound method stub

--- app/src/main/java/ru/byters/blackufaaudio/controllers/ControllerSongs.java
+++ app/src/main/java/ru/byters/blackufaaudio/controllers/ControllerSongs.java
@@ -1,4 +1,15 @@
 package ru.byters.blackufaaudio.controllers;
 
 public class ControllerSongs extends ControllerBase {
+    private static ControllerSongs instance;
+
+    public static ControllerSongs getInstance() {
+        if (instance == null)
+            instance = new ControllerSongs();
+        return instance;
+    }
+
+    public void playRandom() {
+        //todo implement
+    }
 }
