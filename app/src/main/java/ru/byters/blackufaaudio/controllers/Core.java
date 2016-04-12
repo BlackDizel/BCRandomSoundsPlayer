0d5b8012be50013fa3da9700851d6818efe23337
alexandr motorin <blackdizel@gmail.com>
2016-04-12 19:35:11 +0400

    add send feedback and show website methods stubs

--- app/src/main/java/ru/byters/blackufaaudio/controllers/Core.java
+++ app/src/main/java/ru/byters/blackufaaudio/controllers/Core.java
@@ -1,6 +1,14 @@
 package ru.byters.blackufaaudio.controllers;
 
 import android.app.Application;
+import android.content.Context;
 
 public class Core extends Application {
+    public void sendFeedback() {
+        //todo implement
+    }
+
+    public void navigateWebsite(Context context) {
+        //todo implement
+    }
 }
