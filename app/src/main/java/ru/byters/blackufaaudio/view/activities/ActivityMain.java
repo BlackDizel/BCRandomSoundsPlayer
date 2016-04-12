07f86c340066bff771d96acac78210ba1ace061b
alexandr motorin <blackdizel@gmail.com>
2016-04-12 20:41:25 +0400

    implement set state method. override on back pressed

--- app/src/main/java/ru/byters/blackufaaudio/view/activities/ActivityMain.java
+++ app/src/main/java/ru/byters/blackufaaudio/view/activities/ActivityMain.java
@@ -2,28 +2,58 @@ package ru.byters.blackufaaudio.view.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 
 import ru.byters.blackufaaudio.R;
 import ru.byters.blackufaaudio.view.fragments.FragmentMain;
+import ru.byters.blackufaaudio.view.fragments.FragmentSettings;
 
 public class ActivityMain extends ActivityBase {
 
     public static final int STATE_MAIN = 0;
     public static final int STATE_SETTINGS = 1;
     private static final String PAGE_TAG_MAIN = "FRAGMENT_MAIN";
+    private static final String PAGE_TAG_SETTINGS = "FRAGMENT_SETTINGS";
+    private int currentState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment f = getSupportFragmentManager().findFragmentByTag(PAGE_TAG_MAIN);
-        if (f == null) f = FragmentMain.getInstance();
-        f.setRetainInstance(true);
-        getSupportFragmentManager().beginTransaction().replace(R.id.rootView, f, PAGE_TAG_MAIN).commit();
+        setState(STATE_MAIN);
     }
 
-    public void setState(int state){
-        //todo implement
+    public void setState(int state) {
+        Fragment f = null;
+        String tag = null;
+
+        switch (state) {
+            case STATE_MAIN:
+                tag = PAGE_TAG_MAIN;
+                f = getSupportFragmentManager().findFragmentByTag(tag);
+                if (f == null) f = FragmentMain.getInstance();
+                break;
+            case STATE_SETTINGS:
+                tag = PAGE_TAG_SETTINGS;
+                f = getSupportFragmentManager().findFragmentByTag(tag);
+                if (f == null) f = FragmentSettings.getInstance();
+                break;
+        }
+        if (f == null || TextUtils.isEmpty(tag)) return;
+
+        f.setRetainInstance(true);
+        //todo add animation
+        getSupportFragmentManager().beginTransaction().replace(R.id.rootView, f, tag).commit();
+        currentState = state;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (currentState == STATE_SETTINGS)
+            setState(STATE_MAIN);
+        else
+            super.onBackPressed();
     }
 }
