5a79f7285c2a1710641672970310703b3647d5a7
BlackDizel <blackdizel@gmail.com>
2016-04-12 14:10:59 +0300

    implement fragment main layout

--- app/src/main/java/ru/byters/blackufaaudio/view/activities/ActivityMain.java
+++ app/src/main/java/ru/byters/blackufaaudio/view/activities/ActivityMain.java
@@ -1,14 +1,23 @@
 package ru.byters.blackufaaudio.view.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import ru.byters.blackufaaudio.R;
+import ru.byters.blackufaaudio.view.fragments.FragmentMain;
 
 public class ActivityMain extends ActivityBase {
+
+    private static final String PAGE_TAG_MAIN = "FRAGMENT_MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment f = getSupportFragmentManager().findFragmentByTag(PAGE_TAG_MAIN);
+        if (f == null) f = FragmentMain.getInstance();
+        f.setRetainInstance(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.rootView, f, PAGE_TAG_MAIN).commit();
     }
 }
