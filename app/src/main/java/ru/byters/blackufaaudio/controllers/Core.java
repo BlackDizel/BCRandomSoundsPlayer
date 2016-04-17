9712bb6d4b417335ae2260da9f8360ddb87a85f6
BlackDizel <blackdizel@gmail.com>
2016-04-17 20:23:42 +0300

    set core methods static

--- app/src/main/java/ru/byters/blackufaaudio/controllers/Core.java
+++ app/src/main/java/ru/byters/blackufaaudio/controllers/Core.java
@@ -9,45 +9,45 @@ import ru.byters.blackufaaudio.R;
 
 public class Core extends Application {
 
-    public void sendFeedback(Context context) {
-        String uri = getString(R.string.feedback_form_address);
+    public static void sendFeedback(Context context) {
+        String uri = context.getString(R.string.feedback_form_address);
         navigateUri(context, uri);
     }
 
-    public void navigateWebsite(Context context) {
-        String uri = getString(R.string.website_address);
+    public static void navigateWebsite(Context context) {
+        String uri = context.getString(R.string.website_address);
         navigateUri(context, uri);
     }
 
-    private void navigateUri(Context context, String uri) {
+    public static void navigateUri(Context context, String uri) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        if (intent.resolveActivity(getPackageManager()) == null)
+        if (intent.resolveActivity(context.getPackageManager()) == null)
             return;
         context.startActivity(intent);
     }
 
-    public void navigateYouTube(Context context) {
-        String uri = getString(R.string.website_address_channel);
+    public static void navigateYouTube(Context context) {
+        String uri = context.getString(R.string.website_address_channel);
         navigateUri(context, uri);
     }
 
-    public void navigateTwitch(Context context) {
-        String uri = getString(R.string.website_address_twitch);
+    public static void navigateTwitch(Context context) {
+        String uri = context.getString(R.string.website_address_twitch);
         navigateUri(context, uri);
     }
 
-    public void navigateArchive(Context context) {
-        String uri = getString(R.string.website_address_archive);
+    public static void navigateArchive(Context context) {
+        String uri = context.getString(R.string.website_address_archive);
         navigateUri(context, uri);
     }
 
-    public void navigateVK(Context context) {
-        String uri = getString(R.string.website_address_vk);
+    public static void navigateVK(Context context) {
+        String uri = context.getString(R.string.website_address_vk);
         navigateUri(context, uri);
     }
 
-    public void navigateVKByters(Context context) {
-        String uri = getString(R.string.website_address_byters_vk);
+    public static void navigateVKByters(Context context) {
+        String uri = context.getString(R.string.website_address_byters_vk);
         navigateUri(context, uri);
     }
 
